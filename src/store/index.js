9fd44179e612dca57882f573210bc45ac4ea0cd1
rvseanwills <rvb.seanwills@gmail.com>
2021-02-12 12:31:44 +0000

    hotfix3

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex';
 
 import { alert } from './alert.module';
 import { account } from './account.module';
-import { report } from './report.module';
+// import { report } from './report.module';
 import { facebook } from './facebook.module';
 // import { users } from './users.module';
 
@@ -15,7 +15,6 @@ export default new Vuex.Store({
     modules: {
         alert,
         account,
-        report,
         facebook
     }
 });
